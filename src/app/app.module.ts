05a5d3282fc338622560f715e197eb2be0418ff7
Shubham Narang <sh848786@dal.ca>
2019-03-23 19:32:54 -0300

    My feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule , FormsModule} from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ViewChild, ElementRef, NgZone } from '@angular/core';
+
+
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateEventComponent implements OnInit {
 
-  constructor() { }
+EventRegister(form) {
 
-  ngOnInit() {
+   console.log(form.value);
+
+
+
+const req = this.http.post('http://localhost:3000/events', {
+  eventId: 67,
+  imageUrl: "sasas",
+  title: form.value.event_name,
+  description: form.value.event_description,
+  date: form.value.event_date,
+  host: "park victoria",
+  price: 20,
+  category: form.value.category,
+  location: form.value.location
+    })
+      .subscribe(
+        res => {
+          console.log(res);
+        },
+        err => {
+          console.log("Error occured");
+        }
+      );
+
+   console.log(req);
+
+
+
+
+}
+
+
+constructor(private http: HttpClient){
+ }
+
+
+
+ngOnInit() {
+
+
   }
+
+
 
 }
