4bc978719f5812eecf235a89d44c72ed2a596f28
Shubham Narang <sh848786@dal.ca>
2019-03-30 19:16:51 -0300

    added update

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,65 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
-<<<<<<< HEAD
-import { ViewChild, ElementRef, NgZone } from '@angular/core';
-
-
-import { HttpClient } from '@angular/common/http';
-
-=======
 import { HttpClient } from '@angular/common/http';
 
 
->>>>>>> 90ac9a7dd93265f8e0195ab0b3831f66cb50ca2f
+
 @Component({
   selector: 'app-create-event',
   templateUrl: './create-event.component.html',
   styleUrls: ['./create-event.component.css']
 })
+
+
+
 export class CreateEventComponent implements OnInit {
 
-<<<<<<< HEAD
-EventRegister(form) {
 
-   console.log(form.value);
+  constructor(private http: HttpClient){
+   }
 
-
-
-const req = this.http.post('http://localhost:3000/events', {
-  eventId: 67,
-  imageUrl: "sasas",
-  title: form.value.event_name,
-  description: form.value.event_description,
-  date: form.value.event_date,
-  host: "park victoria",
-  price: 20,
-  category: form.value.category,
-  location: form.value.location
-    })
-      .subscribe(
-        res => {
-          console.log(res);
-        },
-        err => {
-          console.log("Error occured");
-        }
-      );
-
-   console.log(req);
-
-
-
-
-}
-
-
-constructor(private http: HttpClient){
- }
-
-
-
-ngOnInit() {
-=======
+  ngOnInit() {}
 
 
 EventRegister(form) {
@@ -91,14 +49,6 @@ EventRegister(form) {
   }
 
 
-
-  constructor(private http: HttpClient){
-   }
-
->>>>>>> 90ac9a7dd93265f8e0195ab0b3831f66cb50ca2f
-
-
-  }
 
 
 
